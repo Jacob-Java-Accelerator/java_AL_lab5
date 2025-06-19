b19c9b4c4d849492fd07c6bc908d83b42d99db7e
roczyno <adiabaroczyno9@gmail.com>
2025-06-19 16:31:02 +0000

    made some changes

---
+++ machine-learning/src/main/java/com/amalitech/ClassifyEmail.java
@@ -0,0 +1,33 @@
+package com.amalitech;
+
+import com.amalitech.model.ModelConfiguration;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.nd4j.linalg.factory.Nd4j;
+
+import java.util.stream.IntStream;
+
+
+public class ClassifyEmail {
+    public static void main(String[] args) {
+        MultiLayerConfiguration multiLayerConfiguration = ModelConfiguration.createMultiLayerConfiguration(1000);
+        MultiLayerNetwork model = new MultiLayerNetwork(multiLayerConfiguration);
+        model.init();
+
+        double[] newEmailFeatures = IntStream.range(0, 1000).mapToDouble(i -> Math.random()).toArray();
+        // Populate the feature vector with appropriate values
+        // Example: random values
+
+        double[][] input = new double[1][];
+        input[0] = newEmailFeatures;
+
+        double[] output = model.output(Nd4j.create(input)).toDoubleVector();
+
+        System.out.println("Spam Probability: " + output[0]);
+        if (output[0] > 0.5) {
+            System.out.println("This is spam!");
+        } else {
+            System.out.println("This is not spam.");
+        }
+    }
+}
